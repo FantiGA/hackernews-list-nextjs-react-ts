6c4500b99e6cc0df9e0035dba1fa06501f5c0e38
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-01-29 23:30:07 +0900

    publish bug fixed

--- docs/_next/static/ki5_GYymfvU8BO7nbg-oQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/755-0ec6505eeff3a1ec.js","static/chunks/pages/index-bd1a0b730d97916a.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+/*
+ * @Author: fantiga
+ * @Date: 2023-01-15 16:57:51
+ * @LastEditTime: 2023-01-29 23:27:39
+ * @LastEditors: fantiga
+ * @FilePath: /hackernews-list-react-ts/next.config.js
+ */
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? './' : '',
 };
 
 module.exports = nextConfig;
