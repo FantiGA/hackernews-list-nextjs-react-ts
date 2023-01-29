ae11aa38c08df922216c50e8ca290b22f52590c0
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-01-29 23:37:41 +0900

    publish bug fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: fantiga
  * @Date: 2023-01-15 16:57:51
- * @LastEditTime: 2023-01-29 23:32:58
+ * @LastEditTime: 2023-01-29 23:37:11
  * @LastEditors: fantiga
  * @FilePath: /hackernews-list-react-ts/next.config.js
  */
@@ -10,7 +10,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://fantiga.github.io/hackernews-list-nextjs-react-ts/' : '',
+  assetPrefix: isProd ? 'https://fantiga.github.io/hackernews-list-nextjs-react-ts/out/' : '',
 };
 
 module.exports = nextConfig;
